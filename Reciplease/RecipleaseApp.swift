a0a105f388f657d59604f338512a7db3efba3f3f
bilal-dallali <dallali-bilal@hotmail.fr>
2024-09-26 19:51:05 +0200

    Initial Commit

---
+++ Reciplease/RecipleaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipleaseApp.swift
+//  Reciplease
+//
+//  Created by Bilal D on 26/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipleaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
