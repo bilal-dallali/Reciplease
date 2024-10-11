893c73644c0aad276ff89936119d542fe4cf42c1
bilal-dallali <dallali-bilal@hotmail.fr>
2024-10-11 22:09:29 +0200

    tab view

--- Reciplease/RecipeListView.swift
+++ Reciplease/RecipeListView.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct RecipeListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("RecipeListView")
+        }
+        .navigationBarBackButtonHidden(true)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color("Background"))
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                BackButtonView()
+            }
+            ToolbarItem(placement: .principal) {
+                Text("Reciplease")
+                    .foregroundStyle(Color("WhiteFont"))
+                    .font(.custom("Gutheng", size: 25))
+            }
+        }
     }
 }
 
--- Reciplease/RecipleaseApp.swift
+++ Reciplease/RecipleaseApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct RecipleaseApp: App {
     var body: some Scene {
         WindowGroup {
-            RecipeSearchView()
+            NavigationStack {
+                RecipeSearchView()
+            }
         }
     }
 }
+
