db97ce40b2d9c02a6fe5e1fbabf5f7a099a90de4
bilal-dallali <dallali-bilal@hotmail.fr>
2024-10-31 23:05:47 +0100

    recipe tab bar

--- Reciplease/RecipleaseApp.swift
+++ Reciplease/RecipleaseApp.swift
@@ -11,9 +11,7 @@ import SwiftUI
 struct RecipleaseApp: App {
     var body: some Scene {
         WindowGroup {
-            NavigationStack {
-                RecipeTabView()
-            }
+            RecipeTabView()
         }
     }
 }
