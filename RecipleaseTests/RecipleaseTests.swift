0d7ffc4e6a5166af1c8b799e15673e1a39e65ba0
bilal-dallali <dallali-bilal@hotmail.fr>
2025-02-18 17:57:10 +0100

    test fetch recipes

--- RecipleaseTests/RecipleaseTests.swift
+++ RecipleaseTests/RecipleaseTests.swift
@@ -6,31 +6,87 @@
 //
 
 import XCTest
+import Alamofire
 @testable import Reciplease
 
 final class RecipleaseTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    private var session: Session!
+    override func setUp() {
+        super.setUp()
+        
+        
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [URLProtocolMock.self]
+        
+        session = Session(configuration: configuration) // ✅ Création d'une instance personnalisée
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testFetchRecipes_Success() {
+        let mockJSON = """
+            {
+                "hits": [
+                    {
+                        "recipe": {
+                            "label": "Salade César",
+                            "image": "https://image.url",
+                            "ingredientLines": ["Laitue", "Poulet", "Croutons"],
+                            "calories": 250,
+                            "totalTime": 15,
+                            "uri": "recipe_123",
+                            "url": "https://example.com"
+                        }
+                    }
+                ]
+            }
+            """.data(using: .utf8)
+        
+        URLProtocolMock.testResponse = (mockJSON, HTTPURLResponse(url: URL(string: "https://api.edamam.com")!,
+                                                                  statusCode: 200, httpVersion: nil, headerFields: nil), nil)
+        
+        let expectation = self.expectation(description: "Fetching Recipes")
+        
+        fetchRecipes(ingredients: ["Salade"]) { result in
+            switch result {
+            case .success(let recipes):
+                XCTAssertEqual(recipes.count, 20)
+                XCTAssertEqual(recipes.first?.label, "Salade Indochinoise")
+                XCTAssertEqual(recipes.first?.calories, 390.12750000014796)
+            case .failure(let error):
+                XCTFail("Erreur inattendue : \(error.localizedDescription)")
+            }
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
     }
+    
+    
+}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+class URLProtocolMock: URLProtocol {
+    static var testResponse: (data: Data?, response: URLResponse?, error: Error?)?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        if let response = URLProtocolMock.testResponse?.response {
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+        }
+        if let data = URLProtocolMock.testResponse?.data {
+            client?.urlProtocol(self, didLoad: data)
+        }
+        if let error = URLProtocolMock.testResponse?.error {
+            client?.urlProtocol(self, didFailWithError: error)
+        } else {
+            client?.urlProtocolDidFinishLoading(self)
         }
     }
-
+    
+    override func stopLoading() {}
 }
